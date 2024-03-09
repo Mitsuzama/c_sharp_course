4e2295b2aff33e8490dc2331de4727fd43f510a5
Elena <elena.chelarasu@yahoo.com>
2024-03-09 14:52:21 +0200

    Create employee factory for school administration

---
+++ SchoolHRAdministration/HRAdministrationAPI/FactoryPattern.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAdministrationAPI
+{
+    public static class FactoryPattern<K,T> where T:class,K,new()
+    {
+        // instantiate an object generically
+        // generics enable the code to be stronglly typed
+        // -> the code is checked at compile time
+        public static K GetInstance()
+        {
+            K objK;
+            objK = new T();
+            return objK;
+        }
+    }
+}
